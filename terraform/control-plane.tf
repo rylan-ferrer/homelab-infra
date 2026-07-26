e75fb9777b8cb12f01eafd9b96585e53d56b9f43
rylan-ferrer <rylanferrer15@gmail.com>
2026-07-25 20:56:52 -0700

    added missing init block and agent to control-plane.tf

--- terraform/control-plane.tf
+++ terraform/control-plane.tf
@@ -4,19 +4,28 @@ resource "proxmox_virtual_environment_vm" "control_plane" {
   node_name = local.control_plane_nodes[count.index]
 
   clone {
-    vm_id = 100
-    node_name = "node3"
+    vm_id        = 100
+    node_name    = "node3"
     datastore_id = var.vm_datastore_id
   }
 
-  lifecycle { 
+  lifecycle {
     ignore_changes = [clone]
   }
+
+  agent {
+    enabled = true
+  }
+
   cpu {
     cores = 2
   }
 
   memory {
     dedicated = 4096
+  }
+
+  initialization {
+    user_data_file_id = proxmox_virtual_environment_file.qemu_agent_config[local.control_plane_nodes[count.index]].id
   }
 }
